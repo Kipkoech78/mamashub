84c6bf9006689dc6a18f0bd12de515af1ccef699
Kipkoech78 <lkipkoech@kabarak.ac.ke>
2024-11-09 12:03:30 +0300

    feat: Update DataClass to include baby teeth record data

--- app/src/main/java/com/kabarak/kabarakmhis/pnc/data_class/DataClass.kt
+++ app/src/main/java/com/kabarak/kabarakmhis/pnc/data_class/DataClass.kt
@@ -5,3 +5,31 @@ data class Child(
     val name: String,
     val birthDate: String,
 )
+data class BabyTeethRecordDataClass(
+    val id: String,
+    val dateSeen: String,
+    val ageWhenSeen: String,
+    val teethType: String?,
+)
+// Model for questionnaire item
+data class QuestionnaireItem(
+    val linkId: String,
+    val text: String,
+    val type: String,
+    val answerOptions: List<AnswerOption>? = null,
+    val subItems: List<QuestionnaireItem>? = null,
+    val enableWhen: List<EnableWhen>? = null
+)
+
+// Model for answer option
+data class AnswerOption(
+    val code: String,
+    val display: String
+)
+
+// Model for enableWhen conditions
+data class EnableWhen(
+    val question: String,
+    val operator: String,
+    val answer: String
+)
